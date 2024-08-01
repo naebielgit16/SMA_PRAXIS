c528b64769bb915370467bcf2f10c3fc74a1f07a
naebielgit16 <naebiel16@gmail.com>
2024-08-01 10:11:36 +0700

    Learn operators in python

---
+++ 01-02-Calculations/operators.py
@@ -0,0 +1,25 @@
+x = 7
+y = 9
+
+# x+=3 is same as a a=a+3
+
+print("First Modulus:", x%y) #modulus pertama nilai x = 7 dan y = 9
+
+print(x)
+
+x += 3
+
+print("Second Modulus:", x%y) #modulus kedua nilai x = 10 dan y = 9
+
+
+#Modulus : sisa dari pembagain
+
+#exponen
+print(x**2) #nilai x = 6
+            # Output = 36
+
+
+#pembulatan ke bawah (floor division) & (Division(Float))
+
+print("Floor Division", 5//3) # Output: 1
+print("Diviosion", 5/3) # Output 1.66666...
