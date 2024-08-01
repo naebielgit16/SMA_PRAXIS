163c93fb90a412161263bb59afa8167154f8bc3e
naebielgit16 <naebiel16@gmail.com>
2024-08-01 10:12:22 +0700

    Learn For loop

---
+++ 01-04-OOP/for.py
@@ -0,0 +1,5 @@
+username = ["Kak Ali", "Kak Galih", "Kak Asma", "Kak Aishah", "Jecy", "Arsya", "Dahayu", "Ardan", "Ilmi", "Nabil", "Fattan", "Faiz", "Fachri"]
+
+for x in username :
+    print("hello")
+    print(x)
