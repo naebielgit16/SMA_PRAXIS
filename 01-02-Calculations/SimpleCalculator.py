78e8bd7c46cb31e32a13cf07815097b6c202c2cb
naebielgit16 <naebiel16@gmail.com>
2024-08-01 10:11:58 +0700

    SimpleCalculator

---
+++ 01-02-Calculations/SimpleCalculator.py
@@ -0,0 +1,9 @@
+number1 = input("Masukkan Angka Pertama :")
+number2 = input("Masukkan Angka kedua :")
+
+n1 = int(number1)
+n2 = int(number2)
+
+hasil = n1 + n2
+print("Hasil dari", n1, "+", n2, "adalah :", hasil)
+
