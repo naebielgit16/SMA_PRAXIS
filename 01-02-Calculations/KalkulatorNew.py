0f83d88d197636813604268531f800dd67447a50
naebielgit16 <naebiel16@gmail.com>
2024-08-01 10:09:07 +0700

    Calculator for level 10

--- 01-02-Calculations/KalkulatorNew.py
+++ 01-02-Calculations/KalkulatorNew.py
@@ -1,53 +1,80 @@
 import os
 os.system('cls')
-os.system('color a')
+#os.system('color a')
 
 username = ["Kak Ali", "Kak Galih", "Kak Asma", "Kak Aishah", "Jecy", "Arsya", "Dahayu", "Ardan", "Ilmi", "Nabil", "Fattan", "Faiz", "Fachri"]
-password = "level 10"
-while True:
-    while True : 
+password = "Praxis"
+
+while True : # to username
         user = input("Masukkan Usename :")
 
         if user.lower() in (name.lower() for name in username) :
-            print("Anda adalah keluarga kelas 10 SMA Praxis")
+            print("Anda adalah salah satu anggota\nkeluarga kelas 10 SMA Praxis \n")
+            print("Selamat Datang Di Kalkulator Kelas 10")
             break
 
         else :
-            print("Anda bukan Keluarga kelas 10 SMA Praxis")
+            print("Anda bukan Keluarga kelas 10 SMA Praxis\nDILARANG MENGGUNAKAN KALKULATOR INI")
             continue
 
-    while True :
-        sandi = input("Masukkan Password :")
+while True : # to Password
+        print("SILAHKAN MASUKKAN PASSWORD")
+        sandi = input("Password :")
 
         if sandi == password :
-            print("Passsword Benar")
+            print("Passsword Benar, lanjutkan...")
             break
         
         else :
-            print("Password salah, coba lagi !")
+            print("Password salah, coba lagi !!")
             continue
+
+while True: # to operation sistem
     
-    while True :
+    while True : # to input first number
         number1 = str(input("Masukkan Angka Pertama :"))
         
         if number1.isnumeric():
-            print(f'Angka yang anda masukkan adalah : {number1}')
+            print(f'Angka Pertama yang anda masukkan adalah : {number1}')
             break
 
         else:
             print("Angka Pertama yang anda masukkan tidak valid, coba lagi.")
             continue
 
-    while True: #operasi
-        operasi = str(input("Masukkan Operasi ('+', '-', '*', '/'):"))
-        op = ["+", "-", "*", "/"]
+    while True: # to select operation
+        print("Silahkan Pilih Operasi")
+        print("'+' = PENJUMLAHAN")
+        print("'-' = PENGURANGAN")
+        print("'*' = PERKALIAN")
+        print("'/' = PEMBAGIAN")
+        print("'^' = PERPANGKATAN")
+        operasi = str(input("Masukkan Operasi ('+', '-', '*', '/', '^'):"))
+        
+        op = ["+", "-", "*", "/","^"]
         if operasi not in op : 
             print("operasi tidak valid")
             continue
-        else: 
+        else : 
             break
 
-    while True :
+    # for Operation options
+    if operasi == "+" : 
+        print("Operasi yang anda pilih adalah PENJUMLAHAN")
+
+    elif operasi == "-" : 
+        print("Operasi yang anda pilih adalah PENGURANGAN")
+
+    elif operasi == "*" :
+        print("Operasi yang anda pilih adalah PERKALIAN")
+
+    elif operasi == "/" :
+        print("Operasi yang anda pilih adalah PEMBAGIAN")
+    
+    elif operasi == "^" :
+        print("Operasi yang anda pilih adalah PERPANGKATAN/EKSPONEN")
+
+    while True : # to input second number
         number2 = str(input("Masukkan Angka Kedua :"))
         
         if number2.isnumeric():
@@ -57,7 +84,7 @@ while True:
             print("Angka yang anda masukkan tidak valid, coba lagi.")
             continue
 
-            
+    # to numeric operation
     if operasi == "+" : 
         result = int(number1) + int(number2)
 
@@ -69,10 +96,14 @@ while True:
 
     elif operasi == "/" :
         result = int(number1) / int(number2)
+    
+    elif operasi == "^" :
+        result = int(number1) ** int(number2)
 
+        result = int
     print(f'Hasil dari {number1} {operasi} {number2} = {result}')
 
-    while True :
+    while True : # to continue the system or not
         ulang = input("Apakah kamu mau berhitung lagi?? (ya/tidak) : ")
 
         if ulang.lower() == "tidak" :
@@ -82,6 +113,7 @@ while True:
             break
 
         else:
+            print("Data tidak valid")
             continue
         
         
