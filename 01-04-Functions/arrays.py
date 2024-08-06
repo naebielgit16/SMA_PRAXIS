7fc1f2c1e5676e645649453dc033b316ffa0b751
naebielgit16 <naebiel16@gmail.com>
2024-08-07 05:42:22 +0700

    about arrys & for loop

---
+++ 01-04-Functions/arrays.py
@@ -0,0 +1,32 @@
+#assign a list
+mylist = ["Faiz", "Fachri", "Fattan"]
+mylist2 = [2,3,4,5,6]
+mylist3 = [1,2,3, "Hallo"]
+
+#print a list
+print(mylist3)
+
+#print spesific member of a list
+print(mylist[0]) #will be print "Faiz"
+
+#sort a list
+mylist.sort
+print(mylist)
+
+#assign a tuble
+mytuple = ("Jecy", "Arsya", "Dahayu")
+
+# try to  change tuple (tidak bisa dirubah)
+    # mytuple[1] = "Hanum"
+
+print(mytuple)
+
+#assign a set
+myset = {"Bayam", "Sawi", "Melon"}
+
+#print a set
+print(myset)
+
+#assign a dictionary
+mydict = {"brand" :"ford", "model" : "mustang", "year" : 1967 }
+print(mydict["model"])
---
+++ 01-04-Functions/forloop.py
@@ -0,0 +1,6 @@
+listplant = ["Bayam", "Sawi", "kangkung"]
+
+print(len(listplant))
+
+for i in listplant: # akan ngeprint sesuai panjang datanya (list, tuple, set, dect)
+    print(i)
